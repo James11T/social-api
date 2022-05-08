048996f4412325974f2e94abb449ebda2fd1a5e5
James11T <jamail090999@gmail.com>
2022-05-09 00:21:13 +0100

    fixed slow test running

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,14 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   verbose: true,
+  maxWorkers: 1,
   transform: {
     "^.+\\.tsx?$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      isolatedModules: true
+    }
   }
 };
 
