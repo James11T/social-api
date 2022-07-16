1b9228def4e83c0d426a25ee8c0c8fe0d51d87e4
James <jamail090999@gmail.com>
2022-07-16 02:58:06 +0100

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import ip from "ip";
+import chalk from "chalk";
 import app from "./app";
 import { RUNTIME_CONSTANTS } from "./constants";
 
 const { PORT, DB_URL } = process.env;
 
 if (RUNTIME_CONSTANTS.IS_DEV) {
-  console.log("Running in development mode");
+  console.log(`${chalk.blue("⚒")} Running in development mode`);
 }
 
+console.log(`${chalk.yellow("⧗")} Starting`);
+
+const link = chalk.blue.underline;
+const success = chalk.green("✓");
+const fail = chalk.red("✗");
+
 mongoose.connect(DB_URL, (error) => {
+  const mongo = chalk.bold.green("MongoDB");
   if (error) {
-    console.error("Failed to connect to MongoDB", error);
+    console.error(`${fail} Failed to connect to ${mongo}`, error);
     return;
   }
 
-  console.log(`Connected to MongoDB`);
+  console.log(`${success} Connected to ${mongo}`);
 
   app.listen(PORT, () => {
     const localAddr = `http://localhost:${PORT}/`;
     const remoteAddr = `http://${ip.address()}:${PORT}/`;
+
     console.log(
-      `\nServer is running on port ${PORT}\n`,
-      `Connect locally with ${localAddr}\n`,
-      `Or on another device with ${remoteAddr}\n`
+      `\n${chalk.green("●")} Server is running on port ${chalk.blue.bold(PORT)}`
     );
+    console.log(`   Connect locally with ${link(localAddr)}`);
+    console.log(`   Or on another device with ${link(remoteAddr)}`);
   });
 });
